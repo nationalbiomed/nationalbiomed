13bb5e48cdf4651f293c3c38e906957108914729
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-15 15:39:49 +0545

    about

--- src/app/(home)/about-us/_components/Introduction.jsx
+++ src/app/(home)/about-us/_components/Introduction.jsx
@@ -1,20 +1,20 @@
-export default function AboutUs() {
+import { Button } from "@/components/ui/button";
+import { ChevronsRight} from "lucide-react";
+export default function Introduction() {
   return (
-    <div className="w-[100%] mb-[100px] lg:mx-[100px] mt-[100px] mx-0 relative overflow-hidden">
-      <div className="md:w-[50%] w-[100%]">
-        <img src="/about.jpg" className=""></img>
-      </div>
-      <div className="bg-green-100 md:p-10 p-5 md:absolute md:w-[40%] w-[100%] md:top-1/2 md:left-[45%] md:transform md:-translate-y-1/2">
-        <h1 className="md:text-3xl text-xl text-green-900 font-bold">
-          
-          Introduction 
-        </h1>
-        <p className="md:text-lg text-md">
+    <div className="w-full mt-[100px] px-20">
+         <h1 className="text-4xl font-bold text-green-600 mb-4">Information</h1>
+
+    <div className="grid grid-cols-2 w-full gap-6 mb-[100px]">
+      <div>
+        <p className="text-sm text-gray-600 mb-5">
           Established in 2008, National Bio-Medical Suppliers is highly reliable
           and reputed importers and suppliers of a vast range of higher-quality
           medical products in Nepal, including hospital/medical/scientific/lab
           equipment, instruments, apparatus, medical disposable, dental
           equipment & materials, surgical instruments, and hospital furniture.
+          </p>
+          <p className="text-sm text-gray-600 mb-5">
           Almost all products, we deal are manufactured by ISO 9001; ISO13485
           certified companies and the products hold CE certification. Certain
           products are also backed up by US FDA510 (K) and WHO
@@ -24,8 +24,14 @@ export default function AboutUs() {
           Nepal, which enable us to provide higher quality products and fastest
           service to our clients through our skilled marketing team and Bio-Med.
           Engineers & Technicians.
+          
         </p>
+        <Button className=" bg-green-600"> Contact us <ChevronsRight/></Button>
       </div>
+      <div >
+        <img src="/aboutimg.jpg" className="rounded-lg"/>
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/app/(home)/about-us/page.jsx
@@ -0,0 +1,11 @@
+
+import Introduction from "./_components/Introduction";
+import Missions from "./_components/Missions";
+export default function AboutUs() {
+  return (
+    <div>
+    <Introduction />
+    <Missions />
+    </div>
+  );
+}
