110d993b0acf1febc27587c1ef2a0013375acd51
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-15 15:47:09 +0545

    try

--- src/app/(home)/about-us/_components/Missions.jsx
+++ src/app/(home)/about-us/_components/Missions.jsx
@@ -3,7 +3,7 @@ export default function Missions(){
     return(
         <div className="grid grid-cols-3">
             <Card>
-                
+                helo
 
             </Card>
             <Card>
