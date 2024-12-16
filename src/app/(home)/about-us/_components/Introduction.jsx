6bbf8b4dda81f2ceb5dcc38e0fcbf7a6f49a2515
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-16 10:45:53 +0545

    mission aos

--- src/app/(home)/about-us/_components/Introduction.jsx
+++ src/app/(home)/about-us/_components/Introduction.jsx
@@ -2,19 +2,21 @@ import { Button } from "@/components/ui/button";
 import { ChevronsRight} from "lucide-react";
 export default function Introduction() {
   return (
-    <div className="w-full mt-[100px] px-20">
-         <h1 className="text-4xl font-bold text-green-600 mb-4">Information</h1>
+    <div className="w-full lg:mt-[100px] mt-[50px] lg:px-20 px-5">
+        
 
-    <div className="grid grid-cols-2 w-full gap-6 mb-[100px]">
-      <div>
-        <p className="text-sm text-gray-600 mb-5">
+    <div className="grid lg:grid-cols-2 grid-cols-1 w-full gap-16 lg:mb-[100px] mb-[50px]">
+      <div  data-aos="fade-right">
+        
+    <h1 className="text-4xl font-bold text-green-600 mb-4">Information</h1>
+        <p className="text-md  text-gray-700 mb-5">
           Established in 2008, National Bio-Medical Suppliers is highly reliable
           and reputed importers and suppliers of a vast range of higher-quality
           medical products in Nepal, including hospital/medical/scientific/lab
           equipment, instruments, apparatus, medical disposable, dental
           equipment & materials, surgical instruments, and hospital furniture.
           </p>
-          <p className="text-sm text-gray-600 mb-5">
+          <p className="text-md text-gray-700 mb-6">
           Almost all products, we deal are manufactured by ISO 9001; ISO13485
           certified companies and the products hold CE certification. Certain
           products are also backed up by US FDA510 (K) and WHO
@@ -29,7 +31,7 @@ export default function Introduction() {
         <Button className=" bg-green-600"> Contact us <ChevronsRight/></Button>
       </div>
       <div >
-        <img src="/aboutimg.jpg" className="rounded-lg"/>
+        <img src="/aboutimg.jpg" className="rounded-lg"  data-aos="fade-left"/>
       </div>
     </div>
     </div>
--- src/app/(home)/about-us/_components/Missions.jsx
+++ src/app/(home)/about-us/_components/Missions.jsx
@@ -1,18 +1,42 @@
 import { Card } from "@/components/ui/card";
-export default function Missions(){
-    return(
-        <div className="grid grid-cols-3">
-            <Card>
-                helo
-
-            </Card>
-            <Card>
-
-            </Card>
-            <Card>
-
-            </Card>
-
+import { Handshake, Heart, Target } from "lucide-react";
+export default function Missions() {
+  return (
+    <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-6 lg:px-24 px-10 mb-[100px]" data-aos="fade-up" >
+      <Card className="w-full text-center p-10 shadow-lg hover:shadow-xl hover:ease-in-out duration-300">
+        <div className="flex justify-center items-center w-full">
+          <Target className=" w-14 h-14 pb-3 text-green-600" />
         </div>
-    )
+        <h1 className="font-semibold text-xl pb-3">Missions</h1>
+        <p className="text-gray-600">
+          Bridge workforce gaps with reliable, skilled, and culturally diverse
+          manpower solutions for the Middle East and Europe, empowering Nepali
+          careers.
+        </p>
+      </Card>
+
+      <Card className="w-full text-center p-10 shadow-lg hover:shadow-xl hover:ease-in-out duration-300">
+        <div className="flex justify-center items-center w-full">
+          <Handshake className=" w-14 h-14 pb-3 text-green-600" />
+        </div>
+        <h1 className="font-semibold text-xl pb-3">Missions</h1>
+        <p className="text-gray-600">
+          Bridge workforce gaps with reliable, skilled, and culturally diverse
+          manpower solutions for the Middle East and Europe, empowering Nepali
+          careers.
+        </p>
+      </Card>
+      <Card className="w-full text-center p-10 shadow-lg hover:shadow-xl hover:ease-in-out duration-300">
+        <div className="flex justify-center items-center w-full">
+          <Heart className=" w-14 h-14 pb-3 text-green-600" />
+        </div>
+        <h1 className="font-semibold text-xl pb-3">Missions</h1>
+        <p className="text-gray-600">
+          Bridge workforce gaps with reliable, skilled, and culturally diverse
+          manpower solutions for the Middle East and Europe, empowering Nepali
+          careers.
+        </p>
+      </Card>
+    </div>
+  );
 }
