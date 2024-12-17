65d9e82231296d7a1c4187ee5d15f94949581e3b
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-17 12:17:49 +0545

    WhyUs responsive

--- src/app/(home)/about-us/page.jsx
+++ src/app/(home)/about-us/page.jsx
@@ -1,11 +1,13 @@
 
 import Introduction from "./_components/Introduction";
 import Missions from "./_components/Missions";
+import WhyUs from "./_components/WhyUs";
 export default function AboutUs() {
   return (
     <div>
     <Introduction />
     <Missions />
+    <WhyUs />
     </div>
   );
 }
