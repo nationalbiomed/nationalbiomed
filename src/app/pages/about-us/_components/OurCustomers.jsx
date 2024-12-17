de88df7d44bc599623b2e3a6fedb67fc09e651f5
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-17 17:13:29 +0545

    delete aboutus and contact

--- src/app/pages/about-us/_components/OurCustomers.jsx
+++ src/app/pages/about-us/_components/OurCustomers.jsx
@@ -2,31 +2,31 @@ const Customers = [
     {
         id:1,
         name:"Department of Health Services",
-        logo: "Emblem_of_Nepal.png",
+        logo: "/Emblem_of_Nepal.png",
         description:"Ministry of Health",
     },
     {
         id:2,
         name:"FHI360",
-        logo: "fhi360-logo.svg",
+        logo: "/fhi360-logo.svg",
         description:"Nepal Country Office",
     },
     {
         id:3,
-        name:"Oppourtunity ",
-        logo: "Emblem_of_Nepal.png",
+        name:"Oppourtunity Village Network",
+        logo: "/OVN.jpg",
         description:"Ministry of Health",
     },
     {
         id:4,
         name:"Department of Health Services",
-        logo: "Emblem_of_Nepal.png",
+        logo: "/Emblem_of_Nepal.png",
         description:"Ministry of Health",
     },
     {
         id:5,
         name:"Department of Health Services",
-        logo: "Emblem_of_Nepal.png",
+        logo: "/Emblem_of_Nepal.png",
         description:"Ministry of Health",
     },
 ]
@@ -40,7 +40,7 @@ export default function OurCustomers(){
        <div className="flex gap-40 text-center animate-marquee mt-10">
             {Customers.map((items)=>(
                 <div key={items.id} className="w-40 flex-shrink-0 text-center mx-auto">
-                    <img src="/Emblem_of_Nepal.png" />
+                    <img src={items.logo} />
                     <h1 className="text-md font-semibold mt-3 text-green-600">{items.name}</h1>
                     <p className="text-gray-600">{items.description}</p>
                     
@@ -49,7 +49,7 @@ export default function OurCustomers(){
             ))}
             {Customers.map((items)=>(
                 <div key={items.id} className="w-40 flex-shrink-0 text-center mx-auto">
-                    <img src="/Emblem_of_Nepal.png" />
+                    <img src={items.logo}  />
                     <h1 className="text-md font-semibold mt-3 text-green-600">{items.name}</h1>
                     <p className="text-gray-600">{items.description}</p>
                     
