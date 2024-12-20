3f3d93c728038759f47781277b14f71883a2a2af
SumanPoU <acharyas186@gmail.com>
2024-12-20 10:35:05 +0545

    changed te main route for ccareer

--- src/app/api/career/route.js
+++ src/app/api/career/route.js
@@ -8,7 +8,23 @@ export async function GET() {
         createdAt: "desc",
       },
     });
-    return NextResponse.json(jobs, { status: 200 });
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Filter jobs that have not passed the `applyBefore` date and map required fields
+    const filteredJobs = jobs
+      .filter(job => !job.applyBefore || new Date(job.applyBefore) > currentDate)
+      .map(job => ({
+        id: job.id,
+        slug: job.slug,
+        title: job.title,
+        type: job.type,
+        level: job.level,
+        applyBefore: job.applyBefore,
+      }));
+
+    return NextResponse.json(filteredJobs, { status: 200 });
   } catch (error) {
     console.error("Error fetching jobs:", error);
     return NextResponse.json({ error: "Error fetching jobs" }, { status: 500 });
