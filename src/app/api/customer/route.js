770a98b6df54e4c8998f44d0a248d93a00dad970
Raj-Stha <rajcrestha00@gmail.com>
2024-12-23 16:48:18 +0545

    Customer API ::FIX

--- src/app/api/customer/route.js
+++ src/app/api/customer/route.js
@@ -1,8 +1,4 @@
 import { NextResponse } from "next/server";
-<<<<<<< HEAD
-=======
-
->>>>>>> abd99bd85cb3bcb452403d15eae741dc994dbfd9
 import db from "@/lib/db";
 
 export async function GET() {
@@ -12,19 +8,10 @@ export async function GET() {
         position: "asc",
       },
     });
-<<<<<<< HEAD
-
-    return NextResponse.json({ data: customer }, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching customer:", error);
-    return NextResponse.json(
-      { error: "Error getting customer" },
-=======
     return NextResponse.json(customer, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: "Error getting Customer" },
->>>>>>> abd99bd85cb3bcb452403d15eae741dc994dbfd9
       { status: 500 }
     );
   }
