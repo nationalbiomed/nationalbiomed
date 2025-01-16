5b6ef02c3a80bb9055704aa5463647c999a29d36
SumanPoU <acharyas186@gmail.com>
2025-01-16 14:11:49 +0545

    changed the team page

--- src/app/pages/team/page.jsx
+++ src/app/pages/team/page.jsx
@@ -1,29 +1,35 @@
 import { Suspense } from "react";
 import Teams from "./_components/Teams";
-import Message from "./_components/Message"
+import Message from "./_components/Message";
 
-export default async function NewsPage({ params }) {
-  let chairman, teamsData; 
-  try{
-    const chairmanRes = await fetch("http://localhost:3000/api/team/get/chairman");
-    if(!chairmanRes.ok) console.log("Error in fetching chairman");
-    chairman= await chairmanRes.json();
+export default async function NewsPage() {
+  let chairman = null;
+  let teamsData = null;
 
-  }catch(error){
+  try {
+    const chairmanRes = await fetch("http://localhost:3000/api/team/get/chairman", { cache: "force-cache" });
+    if (chairmanRes.ok) {
+      chairman = await chairmanRes.json();
+    } else {
+      console.log("Error in fetching chairman");
+    }
+  } catch (error) {
     console.log("Error in chairman", error);
   }
 
-
-  try{
-    const teamRes = await fetch("http://localhost:3000/api/team/makecategory");
-    if(!teamRes.ok) console.log("Error in fetching teams");
-    teamsData= await teamRes.json();
-
-  }catch(error){
+  try {
+    const teamRes = await fetch("http://localhost:3000/api/team/makecategory", { cache: "force-cache" });
+    if (teamRes.ok) {
+      teamsData = await teamRes.json();
+    } else {
+      console.log("Error in fetching teams");
+    }
+  } catch (error) {
     console.log("Error in team", error);
   }
+
   return (
-    <Suspense fallback={<p>loading</p>}>
+    <Suspense fallback={<p>Loading...</p>}>
       <Message chairman={chairman} />
       <Teams teamDataByCategory={teamsData} />
     </Suspense>
